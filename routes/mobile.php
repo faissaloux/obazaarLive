258d1567f70b1550ab7be61144cf6e3ed9f46f95
faissaloux <60013703+faissaloux@users.noreply.github.com>
2021-02-26 18:04:20 +0100

    update: reset password

--- routes/mobile.php
+++ routes/mobile.php
@@ -6,14 +6,17 @@ use Illuminate\Support\Facades\Route;
  * mobile.
  */
 
-Route::view('/',                'mobile/intro'                                  );
-Route::view('/login',           'mobile/login'                                  )->name('login-view'            );
-Route::post('/login',           'MobileControllers\AccountController@userAuth'  )->name('login-auth'            );
-Route::get('/logout',           'Auth\LoginController@logout'                   )->name('logout');
-Route::view('/register',        'mobile/register'                               )->name('register-view'         );
-Route::view('/forget-password', 'mobile/forget-password'                        )->name('forget-password-view'  );
-Route::get('/stores',           'MobileControllers\BaseController@index'        )->name('stores'                );
-Route::view('/orders',          'mobile.orders'                                 )->name('orders'                );
+Route::view('/',                        'mobile/intro'                                      );
+Route::view('/login',                   'mobile/login'                                      )->name('login-view'            );
+Route::post('/login',                   'MobileControllers\AccountController@userAuth'      )->name('login-auth'            );
+Route::get('/logout',                   'Auth\LoginController@logout'                       )->name('logout'                );
+Route::view('/register',                'mobile/register'                                   )->name('register-view'         );
+Route::view('/forget-password',         'mobile/forget-password'                            )->name('forget-password-view'  );
+Route::post('/reset-password',          'MobileControllers\WebsiteController@forgetPassword')->name('forget-password'       );
+Route::any('/reset_password/{token}',   'MobileControllers\WebsiteController@getPassword'   )->name('getPassword'           );
+Route::post('/password/reset',          'MobileControllers\WebsiteController@resetPassword' )->name('password.request'      );
+Route::get('/stores',                   'MobileControllers\BaseController@index'            )->name('stores'                );
+Route::view('/orders',                  'mobile.orders'                                     )->name('orders'                );
 
 Route::group(['prefix' => '{store}', 'as' => 'store.', 'middleware' => 'store'], function(){
     Route::get('/',             'MobileControllers\WebsiteController@home'      );
